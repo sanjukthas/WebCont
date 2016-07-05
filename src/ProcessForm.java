24a6416818645694408643ef9deb4ebb3dad396a
sanjukthas <sanjuktha23@gmail.com>
2016-07-05 10:50:33 -0400

    servlet

---
+++ src/ProcessForm.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProcessForm
+ */
+@WebServlet("/ProcessForm")
+public class ProcessForm extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProcessForm() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		response.setContentType("text/html");
+		String message ="your email is " + email + "<br>" + "your password " +password;
+		PrintWriter out = response.getWriter();
+		out.println(message);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
